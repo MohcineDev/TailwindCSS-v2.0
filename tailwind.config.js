11315f326a5b83e3c32c7593f81dcb8df2d29475
MohcineDev <mouhcinedev@gmail.com>
2020-12-07 09:58:27 +0000

    init files

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge:{
+    enabled: true,
+    content: ['./index.html'],
+  },
+
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
